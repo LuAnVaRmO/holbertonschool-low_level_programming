e3ddc00c52cb7f8aee992705dc18cac4f1c90420
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-06 23:20:22 +0000

    finishing the mandatory tasks

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - If else statement
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+	int sp;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		for (sp = 0; sp <= 9; sp++)
+		{
+			putchar('0' + ch);
+			putchar('0' + sp);
+			if (ch != 9 || sp != 9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
