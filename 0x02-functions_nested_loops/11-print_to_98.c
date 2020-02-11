877a6827010291b1a6a870ca816e61a90342919d
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-11 21:35:06 +0000

    last exercise, i hope that check

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_to_98 - Entry point
+ * @n: Integer Number
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ",n);
+		n++;
+		if (n == 98)
+		{
+			printf("98");
+		}
+	}
+	while (n > 98)
+	{
+		printf("%d, ",n);
+		n--;
+		if (n == 98)
+                {
+                        printf("98");
+                }
+	}
+	putchar('\n');
+}
