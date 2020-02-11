8bd4922d74c8c508bf52b03adb7d4e9b8bb03d40
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-11 22:18:23 +0000

    more fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,23 +8,19 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	while (n <= 97)
 	{
 		printf("%d, ", n);
 		n++;
-		if (n == 98)
-		{
-			printf("98");
-		}
 	}
-	while (n > 98)
+	while (n >= 99)
 	{
 		printf("%d, ", n);
 		n--;
-		if (n == 98)
-		{
-			printf("98");
-		}
+	}
+	if (n == 98)
+	{
+		printf("%d", n);
 	}
 	putchar('\n');
 }
