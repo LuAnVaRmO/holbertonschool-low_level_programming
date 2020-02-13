cabacf6146e693f0668e7135dce2f8265a26e064
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-13 14:12:58 +0000

    is digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - Entry point
+ * @c: Integer Number to evaluate if digit or not
+ * Return: 1 if digit, 0 if otherway
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
