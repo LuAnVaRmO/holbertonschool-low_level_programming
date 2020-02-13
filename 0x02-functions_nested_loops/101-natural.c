7e7b7654871e209a6b1769184e7878159cff7a90
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-02-13 03:51:38 +0000

    fixing ac

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main -  computes and prints the sum of all
- * the multiples of 3 or 5 below 1024 (excluded)
+ * multiples of 3 or 5 below 1024 (excluded)
  * Return: Always 0.
  */
 int main(void)
@@ -10,6 +10,8 @@ int main(void)
 
 	for (i = 0 ; i < 1024 ; i++)
 	{
+		ac = 0;
+
 		if (((i % 3) == 0) || ((i % 5) == 0))
 		{
 			ac += i;
