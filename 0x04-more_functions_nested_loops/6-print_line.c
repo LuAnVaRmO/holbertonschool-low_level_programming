f9022cf3aab2002d874464cab642c950731ee5bc
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-13 20:18:15 +0000

    OMG I have head pain

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_line - Entry point
+ * @n: Integer Number
+ * Return: _ for each n
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
