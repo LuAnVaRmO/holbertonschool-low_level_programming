be8dca891f344507141a9f2817eb1a81385f103d
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-14 01:00:20 +0000

    first commit first advanced

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - find and print the largest prime factor
+ * 
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long n = 612852475143;
+	long d = 2;
+	long p = 0;
+
+	while (n != 1)
+	{
+		if (n % d == 0)
+		{
+			n = n / d;
+			p = d;
+		}
+		d += 1;
+	}
+	printf("%ld\n", p);
+	return (0);
+}
