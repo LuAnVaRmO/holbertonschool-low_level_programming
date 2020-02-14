65efde59f992dc6b8b06618afbe9dd5e958f47c3
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-14 00:31:57 +0000

    fizz buzz filter

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: %3 = Fizz, %5 = Buzz %3%5 = FizzBuzz
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1 ; n <= 100 ; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
