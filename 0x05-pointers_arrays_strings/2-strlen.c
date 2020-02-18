b0e6e7e168ef2dca8d85e7b9920286cbd51a4e8f
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-18 22:50:23 +0000

    second task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - show length of string
+ * @s: character
+ * Description: Show lenght of string
+ * Return: value of lenght
+ */
+int _strlen(char *s)
+{
+	int i;
+	int len;
+
+	for (i = 0 ; s[len] != '\0' ; ++i)
+	{
+		len = i;
+	}
+	return (len);
+}
