93dfdada986831eea668ba80d3fd4a03b95ce4b4
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-19 13:52:31 +0000

    update code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,21 +4,21 @@
  * print_array - prints n elements of array of int followed by a newline
  * @a: pointer to the array
  * @n: size of the array
- * Returns: nothing
+ * Returns: void
  */
 void print_array(int *a, int n)
 {
-	int index;
+	int i;
 
-	for (index = 0; index < n; index++)
+	for (i = 0; i < n; i++)
 	{
-		if (index == 0)
+		if (i == 0)
 		{
 			printf("%d", a[0]);
 		}
 		else
 		{
-			printf(", %d", a[index]);
+			printf(", %d", a[i]);
 		}
 	}
 	printf("\n");
