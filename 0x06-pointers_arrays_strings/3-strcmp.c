53520d1c8fbb811fda19d50b4ed5f29dd2fb10f2
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-20 20:54:01 +0000

    fixing

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,13 @@ int _strcmp(char *s1, char *s2)
 {
 	while (1)
 	{
-		if (*s1 > *s2)
+		if (*s1 < *s2)
 		{
-			return (-15);
+			return (*s1 - *s2);
 		}
-		else if (*s2 > *s1)
+		else if (*s2 < *s1)
 		{
-			return (15);
+			return (*s2);
 		}
 		else
 		{
