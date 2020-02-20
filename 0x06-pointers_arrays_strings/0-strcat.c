ea12c3c032dc65d7ceae9a4b2020ecda1b21a524
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-20 17:57:21 +0000

    modifying betty

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,15 @@ char *_strcat(char *dest, char *src)
 {
 	int len;
 	int i;
-	
+
 	for (i = 0 ; dest[i] != '\0' ; ++i)
 	{
 	}
 	len = i++;
-
 	for (i = 0 ;  src[i] != '\0' ; i++)
 	{
 		dest[len + i] = src[i];
 	}
 	dest[len + i] = '\0';
-	return dest;
+	return (dest);
 }
