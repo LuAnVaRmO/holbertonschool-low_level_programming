2196457c9b6afbc7e835e223bbd1c50a9d038c54
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-20 21:30:34 +0000

    thanks betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i=0;
+	int i = 0;
 	int j;
+
 	while ((s1[i] != s2[i]) || (s1[i] != '\0'))
 	{
-		if ((j = s1[i] - s2[i]) != 0)
+		j = s1[i] - s2[i];
+		if (j != 0)
 		{
 			return (j);
 		}
