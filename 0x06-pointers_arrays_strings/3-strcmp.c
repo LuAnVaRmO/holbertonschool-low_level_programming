dc6f706a8fb5ac874e39576cf3c94ac81424e26c
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-20 21:27:27 +0000

    trying more fixing

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,10 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 == *s2) && (*s1 != '\0'))
+	int i=0;
+	int j;
+	while ((s1[i] != s2[i]) || (s1[i] != '\0'))
 	{
-		++*s1;
-		++*s2;
+		if ((j = s1[i] - s2[i]) != 0)
+		{
+			return (j);
+		}
+		i++;
 	}
-	return (*s1 - *s2);
+	return (0);
 }
