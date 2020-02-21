5e98de6379f3654249633d2f2f8b65f361d1a296
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-02-21 03:49:41 +0000

    to uppercase with ascii

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes lower to upper
+ * @: array of chars
+ * Return: string on uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
