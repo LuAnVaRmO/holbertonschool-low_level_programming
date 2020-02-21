c1a2cf99a19a47f8a6c15f33c1473fae6c919edb
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-02-21 03:51:26 +0000

    fixing betty style

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
  * string_toupper - changes lower to upper
- * @: array of chars
+ * @s: array of chars
  * Return: string on uppercase
  */
 char *string_toupper(char *s)
 {
 	int i;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 97 && s[i] <= 122)
