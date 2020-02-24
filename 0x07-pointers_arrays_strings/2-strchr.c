05cbef0bc3bf8c03874c571326d47cc525841346
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-24 20:09:32 +0000

    saving information

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memcpy - copy str the number of bytes from src to dest
+ * @s: string to search
+ * @c: char to look for
+ * Return: char s
+ */
+char *_strchr(char *s, char c)
+{
+	int i, j;
+
+	for (i = 0; s[i] == c; i++)
+	{
+	}
+	for (j = i ; s[j] != '\0'; j++)
+	{
+	}
+	return (s);
+}
