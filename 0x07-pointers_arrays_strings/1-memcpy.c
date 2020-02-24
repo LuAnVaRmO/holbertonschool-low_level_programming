917b3d2ddf8913b60b9a54caeed139cdde92b436
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-24 19:45:09 +0000

    commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i;
 
-	for (i = 0, j = 0; j <= n && src[i] != '\0'; i++, j++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
