5fcb62581a75382a462e12be705072510de654b6
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-02-25 04:48:56 +0000

    task 8, diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints diagnoal sums
+ * @a: array of int
+ * @size: size of the array
+ * Return: void.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum, sumRev;
+
+	for (i = 0; i < (size * size);)
+	{
+		sum += a[i];
+		i += size + 1;
+	}
+	for (i = size - 1; i < (size * size - 1);)
+	{
+		sumRev += a[i];
+		i += size - 1;
+	}
+	printf("%d, %d\n", sum, sumRev);
+}
