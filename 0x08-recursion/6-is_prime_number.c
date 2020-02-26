6a5244214253d1a2cec3eb4d3320305d89d178a3
Luis Angel Vargas Mosquera <luisangevargas91@gmail.com>
2020-02-26 22:04:50 +0000

    upload verification of prime number or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * is_prime_number - Function that return if an int is prime or not
+ * @n: Int number
+ * Return: integer number
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+	return (prime_num(1, n));
+}
+/**
+ * prime_num - verify number to number if is prime
+ * @x: Int number
+ * @y: int number same to n
+ * Return: integer number
+ */
+int prime_num(int x, int y)
+{
+	if (x * x == y)
+	{
+		return (0);
+	}
+	else if (x * x > y)
+	{
+		return (1);
+	}
+	else
+	{
+		return (prime_num(x + 1, y));
+	}
+}
