115ca585a37b50d90de3f4bccdfebb6fadd0f9d3
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-02-27 02:51:58 +0000

    fixing

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,30 +1,28 @@
 #include "holberton.h"
+
 /**
- * wildcmp - checks two strings
- * @s1: string
- * @s2: string with *
- * Return: 1 if match, otherwise 0
+ * wildcmp - compares two strings, one uses the * wildcard
+ * @s1: first string
+ * @s2: second string, may contain wildcard *
+ * Description: Check if strings s1 and s2 can be considered identical
+ * Return: 1 if match, 0 if not
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2 && *s1 && *s2)
-	{
+	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+/*Check for single character missing or match */
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
 	if (*s1 == '\0' && *s2 == '\0')
-	{
 		return (1);
-	}
+/* Check if string is at end or not.*/
 	if (*s2 == '*')
 	{
 		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-		{
 			return (1);
-		}
-		if (*(s1 + 1) != '\0' && *s2 != '\0')
-		{
+		if (*(s1 + 1) != '\0' && *s2 == '\0')
 			return (0);
-		}
+/* Check for multiple character missing */
 	}
 	return (0);
 }
