6349d445363e3011eec3ed88d69b18f80eff7b47
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-03 21:18:46 -0500

    Betty style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int l1, l2, size;
-	int i;
+	int l1, l2, size, i;
 	char *s;
 
 	if (s1 == NULL)
-	{
 		l1 = 0;
-	}
 	else
 	{
 		for (l1 = 0 ; s1[l1] ; l1++)
-		{
-		}
+			;
 	}
 	if (s2 == NULL)
-	{
 		l2 = 0;
-	}
 	else
 	{
 		for (l2 = 0 ;  s2[l2]; l2++)
-		{
-		}
+			;
 	}
 	size = l1 + l2 + 1;
 	s = malloc(size * sizeof(char));
@@ -49,6 +42,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		s[i + l1] = s2[i];
 	}
-	s[size - 1] = '\0';
+	s[l1 + l2] = '\0';
 	return (s);
 }
