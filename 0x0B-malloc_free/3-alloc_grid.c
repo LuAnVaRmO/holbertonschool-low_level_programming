fe2b487d0d313b26c3d0277b9e9263a5b1e1fa58
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-03 21:49:54 -0500

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - create an grid of 2 dimensions
+ * @width: width of the arrayof characters
+ * @height: height of grid
+ * Description: create a grid bidemensional
+ * Return: int grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, **g;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	g = (int **) malloc(height * sizeof(int *));
+
+	if (g == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < height ; i++)
+	{
+		g[i] = (int *)malloc(sizeof(int) * width);
+		if (g[i] == NULL)
+			return (NULL);
+
+		for (j = 0; j < width ; j++)
+		{
+			g[i][j] = 0;
+		}
+	}
+	return (g);
+}
