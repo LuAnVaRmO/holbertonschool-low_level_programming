558a5d4e9ca00dc0708346c5bb536cb2823fab96
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-05 17:18:05 -0500

    upload task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - allocate memory space and set to 0
+ * @nmemb: numbero of elements
+ * @size: size of bytes
+ * Description: allocate memory
+ * Return: space allocated of memory set to 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
+
+	else
+	{
+		for (i = 0 ; s[nmemb] ; i++)
+		{
+			s[i] = 0;
+		}
+	}
+	return (s);
+}
