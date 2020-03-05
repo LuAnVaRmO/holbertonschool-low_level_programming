20e46e24727dac0bf668ea846211d680ede99f0f
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-05 16:53:32 -0500

    fixing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,13 +42,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s[i] = s1[i];
 	}
-	if (n > l2)
-		n = l2;
-
 	for (i = 0; i < n; i++)
 	{
 		s[i + l1] = s2[i];
 	}
-	s[l1 + l2] = '\0';
+	s[l1 + n] = '\0';
 	return (s);
 }
