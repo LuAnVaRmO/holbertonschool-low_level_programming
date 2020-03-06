f086e0be32e095df9e40c6201af65d749f222448
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-05 22:35:01 -0500

    fixing betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	_memcpy(newptr, ptr, old_size);
-       	free(ptr);
+	free(ptr);
 	return (newptr);
 }
 /**
