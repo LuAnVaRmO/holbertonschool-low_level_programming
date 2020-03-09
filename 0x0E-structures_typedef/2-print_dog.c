f380af8139e1e2c8a7d842708e885815e8bd4c7b
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-09 16:59:41 -0500

    printing fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,6 +5,9 @@
 /**
  * print_dog - print the struct dog
  * @d: the struct dog to print
+ * @n: name
+ * @o: owner
+ * @a: age
  */
 void print_dog(struct dog *d)
 {
@@ -15,11 +18,13 @@ void print_dog(struct dog *d)
 	o = d->owner;
 	a = d->age;
 
-	if (d == NULL)
-		return;
-	if (n == NULL)
-		n = "(nil)";
-	if (o == NULL)
-		o = "(nil)";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", n, a, o);
+	if (d)
+	{
+		if (n == NULL)
+			n = "(nil)";
+		if (o == NULL)
+			o = "(nil)";
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", n, a, o);
+	}
+	return;
 }
