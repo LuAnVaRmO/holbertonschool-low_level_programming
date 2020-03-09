13bbf22ca15c115c99a121b79e6c8a80e130026b
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-09 15:05:50 -0500

    fixing betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - It's a dog
+ * @name: name of the puppy
+ * @age: how many time since birth
+ * @owner: who takes care it
+ * Description: Describe 3 data of the dog
+ */
 struct dog
 {
 	char *name;
