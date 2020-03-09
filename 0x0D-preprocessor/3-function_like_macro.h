d87ee5f94d3c9db9f52db5611f548f23ad8c31f5
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-09 13:39:27 -0500

    fixing

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _ABS_
-#define _ABS_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? -1*(x) : (x))
+#define ABS(x) ((x < 0) ? -(x) : (x))
 
 #endif
