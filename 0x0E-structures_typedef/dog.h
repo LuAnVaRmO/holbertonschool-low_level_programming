93948cb9ee2c023dd549f2e54c7a411ab0b8ad41
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-09 22:01:31 -0500

    add header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 int _strlen(char *s);
+void free_dog(dog_t *d);
 
 #endif
