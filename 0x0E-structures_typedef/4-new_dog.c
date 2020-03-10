8b70317246c8d62ce385d651069365a82c5dbb1c
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-09 22:06:58 -0500

    more fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include "dog.h"
+
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Return: On success 1.
- */
+* new_dog - create a new dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+* Return: pointer to 'dog_t'
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *dog;
+	char *new_name, *new_owner;
+	int i;
 
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (i = 0; name[i]; ++i)
+		;
+	new_name = malloc(sizeof(char) * (i + 1));
+	if (new_name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	while (i >= 0)
+	{
+		new_name[i] = name[i];
+		--i;
+	}
+	for (i = 0; owner[i]; ++i)
+		;
+	new_owner = malloc(sizeof(char) * (i + 1));
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(dog);
+		return (NULL);
+	}
+	while (i >= 0)
+	{
+		new_owner[i] = owner[i];
+		--i;
+	}
+	dog->name = new_name;
+	dog->age = age;
+	dog->owner = new_owner;
+	return (dog);
 }
