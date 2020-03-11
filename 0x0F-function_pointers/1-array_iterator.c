90af977d8393f0c30a4457b47fffbc8f49e1d48d
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-11 13:43:50 -0500

    upload task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an array
+ * @array: Integer array
+ * @size: number of elements of the array
+ * @action: function pointer to use
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (*action)
+	{
+		size_t i;
+		int temp;
+
+		for (i = 0; i < size ; i++)
+		{
+			temp = array[i];
+			action(temp);
+		}
+	}
+}
