a219d4ccc2861e18c852f8969a12a645e6dad39a
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-11 14:16:16 -0500

    upload task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an array
+ * @array: Integer array
+ * @size: number of elements of the array
+ * @cmp: function pointer to use
+ * Return: index of the same element.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, temp;
+
+	if (array)
+	{	if (size)
+		{
+			if (cmp)
+			{
+				for (i = 0; i < size ; i++)
+				{
+					temp = cmp(array[i]);
+
+					if (temp != 0)
+					{
+						return (i);
+					}
+				}
+			}
+		}
+	}
+	return (-1);
+}
