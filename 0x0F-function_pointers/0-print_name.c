d01bf8d5d6c27cee1fc08af787811b7279a61eed
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-11 12:54:37 -0500

    upload header and 0 task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: the function pointer to use
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (*f)
+	{
+		void (*pr)(char *);
+
+		pr = f;
+		pr(name);
+	}
+}
