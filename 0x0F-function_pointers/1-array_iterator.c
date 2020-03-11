6f1712323d2da1bc53422e5f7b132f3876649095
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-11 13:53:11 -0500

    fixing

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,9 +14,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
        	size_t i;
 	int n;
 
-	for (i = 0; i < size ; i++)
+	if(*action)
 	{
-		n = array[i];
-		action(n);
+		for (i = 0; i < size ; i++)
+		{
+			n = array[i];
+			action(n);
+		}
 	}
 }
