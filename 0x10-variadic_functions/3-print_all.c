800517a63f66021e0d795b55caa5b7c4e70674e8
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-14 18:05:42 -0500

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,42 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-/**
- * print_char- print char
- * @c: char
- */
-void print_char(char c)
-{
-	printf("%c", c);
-}
-/**
- * print_integer - print int
- * @n: integer number
- */
-void print_integer(int n)
-{
-	printf("%d", n);
-}
-/**
- * print_float - print float
- * @f: float number
- */
-void print_float(float f)
-{
-	printf("%f", f);
-}
-/**
- * print_string - string
- * @s: string
- */
-void print_string(char *s)
-{
-	while (s == NULL)
-	{
-		s = "(nil)";
-		break;
-	}
-	printf("%s", s);
-}
 /**
  * print_all - print all values on any format
  * @format: format to print
@@ -45,6 +7,7 @@ void print_string(char *s)
  */
 void print_all(const char * const format, ...)
 {
+	char *s;
 	int i = 0, ignore = 0;
 	va_list ap;
 
@@ -57,29 +20,35 @@ void print_all(const char * const format, ...)
 			switch (*(format + i))
 			{
 			case 'c':
-				print_char(va_arg(ap, int));
+				printf("%c", va_arg(ap, int));
 				break;
 			case 'i':
-				print_integer(va_arg(ap, int));
+				printf("%d", va_arg(ap, int));
 				break;
 			case 'f':
-				print_float(va_arg(ap, double));
+				printf("%f", va_arg(ap, double));
 				break;
 			case 's':
-				print_string(va_arg(ap, char *));
+				s = va_arg(ap, char *);
+				while (s == NULL)
+				{
+					s = "(nil)";
+					break;
+				}
+				printf("%s", s);
 				break;
 			default:
 				ignore = 1;
 				break;
 			}
+			if (*(format + i + 1) != 0 && ignore == 0)
+			{
+				printf(", ");
+			}
+			i++;
+			ignore = 0;
 		}
-		if (*(format + i + 1) != 0 && ignore == 0)
-		{
-			printf(", ");
-		}
-		i++;
-		ignore = 0;
 	}
-	putchar('\n');
 	va_end(ap);
+	printf("\n");
 }
