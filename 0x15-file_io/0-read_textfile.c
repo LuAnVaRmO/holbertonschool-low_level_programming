a50b778063612b0eaa35765bed6a8d5aa8fef78c
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-31 16:04:52 -0500

    first task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,17 +17,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
+	{
+		close(fd);
 		return (0);
-
+	}
 	sterr = read(fd, buff, letters);
+	close(fd);
 	if (sterr == -1)
+	{
+		free(buff);
 		return (0);
-	msge = write(1, buff, sterr);
+	}
+	msge = write(STDOUT_FILENO, buff, sterr);
 	if (msge == -1)
 		return (0);
 	if (msge != sterr)
 		return (0);
 	free(buff);
-	close(fd);
 	return (msge);
 }
