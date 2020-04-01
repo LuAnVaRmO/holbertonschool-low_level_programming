e6cd91aa3398c2e20604a5d18d317e80f6ebf852
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-03-31 20:58:20 -0500

    repairing code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,21 @@
 #include "holberton.h"
 /**
- * append_text_to_file - append text to end of a file
- * @filename: the name to the file to read
- * @text_content: the strig to contain the file
- * Return: 1 if good or -1 if fail
+  * printerror -  print errors
+  * @message: error message to print
+  * @file: the file name
+  * @code: the exit status
+  * Return: void
+  */
+void printerror(char *message, char *file, int code)
+{
+	dprintf(STDERR_FILENO, "%s%s\n", message, file);
+	exit(code);
+}
+/**
+ * main - Entry point
+ * @argc: the size of arguments
+ * @argv: the arguments
+ * Return: 0 succes
  */
 int main(int argc, char *argv[])
 {
@@ -17,36 +29,21 @@ int main(int argc, char *argv[])
 	}
 	fdsrc = open(argv[1], O_RDONLY);
 	if (fdsrc == -1)
-	{
-		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't read from file ", argv[1]);
-		exit(98);
-	}
+		printerror("Error: Can't read from file ", argv[1], 98);
 	fddest = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 00664);
-	if ( fddest == -1)
-	{
-		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't write to ", argv[2]);
-		exit(99);
-	}
+	if (fddest == -1)
+		printerror("Error: Can't write to ", argv[2], 99);
 	vread = 1024;
 	while (vread == 1024)
 	{
 		vread = read(fdsrc, buffer, 1024);
 		if (vread == -1)
-		{
-			dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't read from file ", argv[1]);
-			exit(98);
-		}
+			printerror("Error: Can't read from file ", argv[1], 98);
 		vwrite = write(fddest, buffer, vread);
 		if (vwrite == -1)
-		{
-			dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't write to ", argv[2]);
-			exit(99);
-		}
+			printerror("Error: Can't write to ", argv[2], 99);
 		if (vwrite != vread)
-		{
-			dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't write to ", argv[2]);
-			exit(99);
-		}
+			printerror("Error: Can't write to ", argv[2], 99);
 	}
 	if (close(fdsrc))
 	{
