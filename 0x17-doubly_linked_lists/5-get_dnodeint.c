236e58365a81eabdac75f9adc6e4e07e36641b01
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-02 21:24:06 -0500

    fixing code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int count = 0;
 	dlistint_t *temp = head;
 
-	while (head)
+	while (temp)
 	{
 		if (count == index)
 			return (temp);
