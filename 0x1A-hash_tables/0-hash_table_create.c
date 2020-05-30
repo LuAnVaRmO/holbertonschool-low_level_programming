90c5c32c889c40a5d25ec0b3944d7d4b2016f964
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-29 19:52:31 -0500

    first commit hash table project

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_table.h"
+/**
+ * hash_table_create - Function To create a hash table
+ * @size: the size of the array
+ * Return: a hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h_table;
+	unsigned int i = 0;
+
+	if (size == 0 || size == '\0')
+	{
+		return (NULL);
+	}
+	h_table = calloc(1, sizeof(hash_table_t));
+	if (h_table == NULL)
+	{
+		free(h_table);
+		return (NULL);
+	}
+	h_table->size = size;
+	h_table->array = calloc(1, sizeof(hash_node_t));
+	if (h_table->array == NULL)
+	{
+		free(h_table);
+		return (NULL);
+	}
+
+	return (h_table);
+}
