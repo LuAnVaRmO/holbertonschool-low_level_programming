e1faf90464f59311e4811be3d2452cc768bc8043
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-29 22:10:16 -0500

    fixing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,30 +11,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_h, *temp;
 
-	if (ht == NULL || key == NULL || value == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index == 0 || index == '\0')
-		return (0);
-	temp = ht->array[index];
-	while (temp)
+	if (ht)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (ht == NULL || key == NULL || value == NULL)
+			return (0);
+		index = key_index((const unsigned char *)key, ht->size);
+		temp = ht->array[index];
+		while (temp)
 		{
-			free(temp->value);
-			temp->value = strdup(value);
-			return (1);
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(temp->value);
+				temp->value = strdup(value);
+				return (1);
+			}
+			temp = temp->next;
 		}
-		temp = temp->next;
+		new_h = malloc(sizeof(hash_node_t));
+		if (new_h == NULL)
+			return (0);
+		new_h->key = strdup(key);
+		new_h->value = strdup(value);
+		if (new_h->key == NULL || new_h->value == NULL)
+			return (0);
+		new_h->next = ht->array[index];
+		ht->array[index] = new_h;
+		return (1);
 	}
-	new_h = malloc(sizeof(hash_node_t));
-	if (new_h == NULL)
-		return (0);
-	new_h->key = strdup(key);
-	new_h->value = strdup(value);
-	if (new_h->key == NULL || new_h->value == NULL)
-		return (0);
-	new_h->next = ht->array[index];
-	ht->array[index] = new_h;
-	return (1);
+	return (0);
 }
