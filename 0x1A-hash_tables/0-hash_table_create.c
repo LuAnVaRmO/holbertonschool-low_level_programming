0e6eaf6c6a55c6e31c599553a32de30286bb44de
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-29 20:00:16 -0500

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,12 +19,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	h_table->size = size;
-	h_table->array = calloc(1, sizeof(hash_node_t));
+	h_table->array = calloc(size, sizeof(hash_node_t *));
 	if (h_table->array == NULL)
 	{
 		free(h_table);
 		return (NULL);
 	}
-
 	return (h_table);
 }
